2a38b7c41cf7bf88abbb9854f9254eb1fcbacdf7
Liam Houghton <liamhoughton90@yahoo.com>
2018-02-12 08:38:43 -0600

    added boostrap stuff

--- TrashWebsite/App_Start/BundleConfig.cs
+++ TrashWebsite/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TrashWebsite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-super.css",
                       "~/Content/site.css"));
         }
     }
